7bebbc8f9f91f934b8073247a5f4aa346e01d66e
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-09-19 23:37:39 -0400

    Update article.js

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Anthony Vu`
   return (
     <motion.article
       initial="hidden"
